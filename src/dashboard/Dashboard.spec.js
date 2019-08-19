f55696833ffcf4c59bc19bf86bfe12fc5252b3a9
Your Name <you@example.com>
2019-08-19 14:03:01 -0700

    done test Dashboard shows the controls and display

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
-// Test away
+import React from 'react';
+import renderer from "react-test-renderer";
+import Dashboard from "./Dashboard"
+
+describe("<Dashboard/>", ()=> {
+    it("shows the controls and display", ()=> {
+        const tree = renderer.create(<Dashboard />)
+        console.log('tree', tree)
+        expect(tree.toJSON()).toMatchSnapshot()
+    })
+})
