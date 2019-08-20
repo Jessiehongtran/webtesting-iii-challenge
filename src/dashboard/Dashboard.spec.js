bd28a915f0b2e5ade2d7f888cec792dcddc95d11
Your Name <you@example.com>
2019-08-19 17:20:19 -0700

    done most of tests except test for button's text change

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,6 +3,7 @@ import renderer from "react-test-renderer";
 import Dashboard from "./Dashboard"
 
 describe("<Dashboard/>", ()=> {
+    //Test Dashboard shows the controls and display
     it("shows the controls and display", ()=> {
         const tree = renderer.create(<Dashboard />)
         console.log('tree', tree)
